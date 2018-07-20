460864473a02d6db70d7bb421b365018d8508941
walk4U <498753250@qq.com>
2018-07-20 16:22:43 +0800

    add 简单的index页面，配置静态资源访问

--- src/main/java/com/jia/controller/IndexController.java
+++ src/main/java/com/jia/controller/IndexController.java
@@ -1,15 +1,14 @@
 package com.jia.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = {"/","/index"}, method = RequestMethod.GET)
     public String sayHello() {
-        return "Hello SpringBoot!";
+        return "index";
     }
 }
