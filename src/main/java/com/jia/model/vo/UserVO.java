b8676b8995ec5f22ae651672d04076c84fe5c2d2
walk4U <498753250@qq.com>
2018-07-24 15:45:06 +0800

    update 修改目录结构

---
+++ src/main/java/com/jia/model/vo/UserVO.java
@@ -0,0 +1,9 @@
+package com.jia.model.vo;
+
+/**
+ * @Auther: jia
+ * @Date: 2018/7/24 15:28
+ * @Description:
+ */
+public class UserVO {
+}
---
+++ src/main/java/com/jia/shiro/ShiroUtil.java
@@ -0,0 +1,9 @@
+package com.jia.shiro;
+
+/**
+ * @Auther: jia
+ * @Date: 2018/7/24 13:44
+ * @Description:
+ */
+public class ShiroUtil {
+}
